b09388dd8ebfb47d55842a3cac12801ebeec78d7
weibh <weibaohui@gmail.com>
2018-11-03 14:16:06 +0800

    init 1.12.2

--- install-docker.sh
+++ install-docker.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-# install docker
+#指定安装的docker 版本 #https://docs.docker.com/install/linux/docker-ce/centos/#install-docker-ce-1
 sudo rm -rf /etc/yum.repos.d/docker*.repo
 
 if [ "$1" == "new" ]; then
 	sudo systemctl stop docker
 	sudo yum -y remove docker*
+	sudo rm -rf /var/lib/docker/
 	sudo yum install -y yum-utils  device-mapper-persistent-data  lvm2
 	sudo yum-config-manager  --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
 	sudo yum clean all
-	sudo yum -y install docker-ce
+	sudo yum -y install docker-ce-17.12.1
 	sudo mkdir -p /etc/docker/
 else
 	sudo yum -y install docker
@@ -22,5 +23,4 @@ sudo cat > /etc/docker/daemon.json <<EOF
 EOF
 
 sudo systemctl enable docker && sudo systemctl start docker
-
 
