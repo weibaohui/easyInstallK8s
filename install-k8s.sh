179694660d70522c6a0b8d1144d5731cc3f1f267
weibh <weibaohui@gmail.com>
2018-12-16 20:56:15 +0800

    1.13.1

--- install-k8s.sh
+++ install-k8s.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 #yum 安装 k8s组件 ，阿里云的源
-#VERSION="1.12.2"
 VERSION=$1
 
 #删除原来安装的版本
@@ -22,13 +21,18 @@ gpgkey=http://mirrors.aliyun.com/kubernetes/yum/doc/yum-key.gpg
         http://mirrors.aliyun.com/kubernetes/yum/doc/rpm-package-key.gpg
 EOF
 
-sudo yum install -y kubelet-$VERSION kubeadm-$VERSION kubectl-$VERSION --disableexcludes=kubernetes 
+sudo yum install -y kubelet-$VERSION kubeadm-$VERSION kubectl-$VERSION --disableexcludes=kubernetes  ipvsadm
 sudo systemctl enable kubelet
 
 sudo cat <<EOF >  /etc/sysctl.d/k8s.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
 EOF
+
+
+
+
+
 
 DOCKER_CGROUPS=$(docker info | grep 'Cgroup' | cut -d' ' -f3)
 echo $DOCKER_CGROUPS
--- setupconfig.sh
+++ setupconfig.sh
@@ -16,3 +16,28 @@ sudo setenforce 0
 sudo sed -i "s/SELINUX=enforcing/SELINUX=disabled/g" /etc/selinux/config
 sudo sed -i "s/SELINUX=permissive/SELINUX=disabled/g" /etc/selinux/config
 
+# 同步时间
+yum install -y ntpdate
+ntpdate -u ntp.api.bz
+
+# 升级内核
+uname -sr
+rpm -Uvh http://www.elrepo.org/elrepo-release-7.0-2.el7.elrepo.noarch.rpm ;
+yum --enablerepo=elrepo-kernel install kernel-ml-devel kernel-ml -y
+uname -sr
+# 设置 GRUB 默认的内核版本 nano /etc/default/grub 设置GRUB_DEFAULT=0，执行grub2-mkconfig -o /boot/grub2/grub.cfg&&reboot
+
+
+# 确认内核版本后，开启IPVS
+uname -sr
+cat > /etc/sysconfig/modules/ipvs.modules <<EOF
+#!/bin/bash
+ipvs_modules="ip_vs ip_vs_lc ip_vs_wlc ip_vs_rr ip_vs_wrr ip_vs_lblc ip_vs_lblcr ip_vs_dh ip_vs_sh ip_vs_fo ip_vs_nq ip_vs_sed ip_vs_ftp nf_conntrack"
+for kernel_module in \${ipvs_modules}; do
+ /sbin/modinfo -F filename \${kernel_module} > /dev/null 2>&1
+ if [ $? -eq 0 ]; then
+ /sbin/modprobe \${kernel_module}
+ fi
+done
+EOF
+chmod 755 /etc/sysconfig/modules/ipvs.modules && bash /etc/sysconfig/modules/ipvs.modules && lsmod | grep ip_vs
