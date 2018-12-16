0df1509ceb5e160f8de7d7bed6ea483de6ed4e50
weibh <weibaohui@gmail.com>
2018-12-16 15:45:17 +0800

    1.13.1

--- prepare/pullk8s.gcr.io.sh
+++ prepare/pullk8s.gcr.io.sh
@@ -1,37 +1,37 @@
 #!/usr/bin/env bash
 #先翻墙把所有的都下载下来
 docker pull k8s.gcr.io/etcd:3.2.24
-docker pull k8s.gcr.io/kube-apiserver:v1.12.2
-docker pull k8s.gcr.io/kube-controller-manager:v1.12.2
-docker pull k8s.gcr.io/kube-scheduler:v1.12.2
-docker pull k8s.gcr.io/kube-proxy:v1.12.2
-docker pull k8s.gcr.io/coredns:1.2.2
+docker pull k8s.gcr.io/kube-apiserver:v1.13.1
+docker pull k8s.gcr.io/kube-controller-manager:v1.13.1
+docker pull k8s.gcr.io/kube-scheduler:v1.13.1
+docker pull k8s.gcr.io/kube-proxy:v1.13.1
+docker pull k8s.gcr.io/coredns:1.2.6
 docker pull k8s.gcr.io/pause:3.1
 docker pull k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0
 docker pull k8s.gcr.io/metrics-server-amd64:v0.2.1
-docker pull k8s.gcr.io/addon-resizer:1.8.1
+docker pull k8s.gcr.io/addon-resizer:1.8.4
 docker pull quay.io/coreos/flannel:v0.10.0-amd64
 #发送到镜像仓库
 docker tag k8s.gcr.io/etcd:3.2.24 weibh/etcd:3.2.24
-docker tag k8s.gcr.io/kube-apiserver:v1.12.2 weibh/kube-apiserver:v1.12.2
-docker tag k8s.gcr.io/kube-controller-manager:v1.12.2 weibh/kube-controller-manager:v1.12.2
-docker tag k8s.gcr.io/kube-scheduler:v1.12.2 weibh/kube-scheduler:v1.12.2
-docker tag k8s.gcr.io/kube-proxy:v1.12.2 weibh/kube-proxy:v1.12.2
-docker tag k8s.gcr.io/coredns:1.2.2 weibh/coredns:1.2.2
+docker tag k8s.gcr.io/kube-apiserver:v1.13.1 weibh/kube-apiserver:v1.13.1
+docker tag k8s.gcr.io/kube-controller-manager:v1.13.1 weibh/kube-controller-manager:v1.13.1
+docker tag k8s.gcr.io/kube-scheduler:v1.13.1 weibh/kube-scheduler:v1.13.1
+docker tag k8s.gcr.io/kube-proxy:v1.13.1 weibh/kube-proxy:v1.13.1
+docker tag k8s.gcr.io/coredns:1.2.6 weibh/coredns:1.2.6
 docker tag k8s.gcr.io/pause:3.1 weibh/pause:3.1
 docker tag k8s.gcr.io/kubernetes-dashboard-amd64:v1.10.0 weibh/kubernetes-dashboard-amd64:v1.10.0
-docker tag k8s.gcr.io/metrics-server-amd64:v0.2.1 weibh/metrics-server-amd64:v0.2.1
+docker tag k8s.gcr.io/metrics-server-amd64:v0.3.1 weibh/metrics-server-amd64:v0.3.1
 docker tag k8s.gcr.io/addon-resizer:1.8.1 weibh/addon-resizer:1.8.1
 docker tag quay.io/coreos/flannel:v0.10.0-amd64 weibh/flannel:v0.10.0-amd64
 
 docker push weibh/etcd:3.2.24
-docker push weibh/kube-apiserver:v1.12.2
-docker push weibh/kube-controller-manager:v1.12.2
-docker push weibh/kube-scheduler:v1.12.2
-docker push weibh/kube-proxy:v1.12.2
-docker push weibh/coredns:1.2.2
+docker push weibh/kube-apiserver:v1.13.1
+docker push weibh/kube-controller-manager:v1.13.1
+docker push weibh/kube-scheduler:v1.13.1
+docker push weibh/kube-proxy:v1.13.1
+docker push weibh/coredns:1.2.6
 docker push weibh/pause:3.1
 docker push weibh/flannel:v0.10.0-amd64
 docker push weibh/kubernetes-dashboard-amd64:v1.10.0
-docker push weibh/addon-resizer:1.8.1
-docker push weibh/metrics-server-amd64:v0.2.1
+docker push weibh/addon-resizer:1.8.4
+docker push weibh/metrics-server-amd64:v0.3.1
--- pull-docker.sh
+++ pull-docker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker pull weibh/etcd:3.2.24
-docker pull weibh/kube-apiserver:v1.12.2
-docker pull weibh/kube-controller-manager:v1.12.2
-docker pull weibh/kube-scheduler:v1.12.2
-docker pull weibh/kube-proxy:v1.12.2
+docker pull weibh/kube-apiserver:v1.13.1
+docker pull weibh/kube-controller-manager:v1.13.1
+docker pull weibh/kube-scheduler:v1.13.1
+docker pull weibh/kube-proxy:v1.13.1
 docker pull weibh/coredns:1.2.2
 docker pull weibh/pause:3.1
 docker pull weibh/flannel:v0.10.0-amd64
@@ -13,10 +13,10 @@ docker pull weibh/metrics-server-amd64:v0.2.1
 docker pull weibh/addon-resizer:1.8.1
 
 docker tag weibh/etcd:3.2.24 k8s.gcr.io/etcd:3.2.24
-docker tag weibh/kube-apiserver:v1.12.2 k8s.gcr.io/kube-apiserver:v1.12.2
-docker tag weibh/kube-controller-manager:v1.12.2 k8s.gcr.io/kube-controller-manager:v1.12.2
-docker tag weibh/kube-scheduler:v1.12.2 k8s.gcr.io/kube-scheduler:v1.12.2
-docker tag weibh/kube-proxy:v1.12.2 k8s.gcr.io/kube-proxy:v1.12.2
+docker tag weibh/kube-apiserver:v1.13.1 k8s.gcr.io/kube-apiserver:v1.13.1
+docker tag weibh/kube-controller-manager:v1.13.1 k8s.gcr.io/kube-controller-manager:v1.13.1
+docker tag weibh/kube-scheduler:v1.13.1 k8s.gcr.io/kube-scheduler:v1.13.1
+docker tag weibh/kube-proxy:v1.13.1 k8s.gcr.io/kube-proxy:v1.13.1
 docker tag weibh/coredns:1.2.2 k8s.gcr.io/coredns:1.2.2
 docker tag weibh/pause:3.1 k8s.gcr.io/pause:3.1
 docker tag weibh/flannel:v0.10.0-amd64 quay.io/coreos/flannel:v0.10.0-amd64
