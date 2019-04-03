cf9961104f001bc4ded3a93913899658a18368b8
weibaohui <weibaohui@yeah.net>
2019-04-03 14:24:51 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -10,7 +10,7 @@ if [ "$1" == "new" ]; then
 	sudo yum install -y yum-utils  device-mapper-persistent-data  lvm2
 	sudo yum-config-manager  --add-repo  https://download.docker.com/linux/centos/docker-ce.repo
 	sudo yum clean all
-	sudo yum -y install docker-ce-18.06.1.ce
+	sudo yum -y install docker-ce-18.09.4.ce
 	sudo mkdir -p /etc/docker/
 else
 	sudo yum -y install docker
@@ -19,7 +19,15 @@ fi
 sudo mkdir -p /etc/docker/
 sudo cat > /etc/docker/daemon.json <<EOF
 {
-  "registry-mirrors": ["http://hub-mirror.c.163.com","https://f9pifyym.mirror.aliyuncs.com","https://registry.docker-cn.com"]
+  "registry-mirrors": ["http://hub-mirror.c.163.com","https://f9pifyym.mirror.aliyuncs.com","https://registry.docker-cn.com"],
+  "max-concurrent-downloads": 3,
+  "max-concurrent-uploads": 5,
+  #"insecure-registries": ["192.168.1.100","IP:PORT"],
+  "log-driver": "json-file",
+  "log-opts": {
+    "max-size": "100m",
+    "max-file": "3"
+    }
 }
 EOF
 
