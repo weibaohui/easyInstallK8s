5a9b3aebdaa64197b5cb395de6debcb7427c5575
weibaohui <weibaohui@yeah.net>
2019-04-03 14:10:37 +0800

    1.14.0

--- node-template.sh
+++ node-template.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # cd 到目录中运行 ./install-node.sh 就可以了
-K8sVersion="1.12.2"
+K8sVersion="1.14.0"
 echo -e "\033[32m## 初始化k8s所需要环境. =====================================================================\033[0m"
 /bin/bash setupconfig.sh
 echo -e "\033[32m## 安装docker,如果不需要请注释该行,安装新版docker修改下面这句 /bin/bash install-docker.sh ===\033[0m"
